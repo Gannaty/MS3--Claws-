d1e567bb9d2aac1d20fad53d0c97014b1dfd8de9
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-08-13 15:09:39 +0000

    Created Flask App

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import os
 
+# Import flask
 from flask import Flask
 
+# Importing env only if the os can find the existing file path
+if os.path.exists("env.py"):
+    import env
+
+
+# Creating an instance of Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "hello World...again!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
