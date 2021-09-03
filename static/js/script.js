0df7289bad0b23cc9c365625987af5038dee1045
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2021-09-03 11:07:26 +0000

    Copied add posts bootstrap code and adjusted for edit posts

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-});
-
-
-//Character limit for post captions
-$(document).ready(function() {
-    $('textarea#textarea2').characterCounter();
-  });
 
 
 //Materialize modal trigger
